702933cae2ae561a945abd9ce4c17860e0af24c1
barakadax <barakadax@gmail.com>
2026-03-07 17:27:21 +0200

    Excel

---
+++ excel column name/readme.md
@@ -0,0 +1,3 @@
+# Excel column name
+
+Given number N you want to calculate the relevant column number in Excel, 1 == A, 2 == B etc
---
+++ excel column name/s.py
@@ -0,0 +1,18 @@
+def colName(n: int) -> str:
+    res: list[str] = []
+
+    while n > 0:
+        rem: int = n % 26
+
+        if rem == 0:
+            res.append('Z')
+            n = (n // 26) - 1
+
+        else:
+            res.append(chr((rem - 1) + ord('A')))
+            n //= 26
+
+    return ''.join(res)
+
+n = 108
+print(colName(n)) ## DD
